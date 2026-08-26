82cee7e83eccdb45d4e6f2379832efb713f2efc3
smileyharini424-cloud <smileyharini424@gmail.com>
2026-08-25 23:09:49 -0700

    Create count_even_odd_elements.c

---
+++ count_even_odd_elements.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+    int even = 0, odd = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    printf("Number of even elements = %d\n", even);
+    printf("Number of odd elements = %d\n", odd);
+
+    return 0;
+}
